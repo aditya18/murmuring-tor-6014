697017daac9d10e7d5ca58f16076df7297117e66
aditya18 <aditya18@gmail.com>
2013-11-15 18:07:31 +0530

    adi changes again

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
-
-<!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=yes" />
-
+   
     <title>Hello! Welcome ADI</title>
-    dif]-->
-  </head>
+    </head>
   <body>
   Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! Dude this is awesome!!!!!!! 
  
