7b73dc5cddedd55fbdc32efe71b2c227f5b7a219
aditya18 <aditya18@gmail.com>
2015-06-24 16:05:02 +0530

    test 1234
    test

--- code/getBday.php
+++ code/getBday.php
@@ -13,19 +13,50 @@ if($day < 10)
 
 $bday = $mon . $day . ".html";
 
-$url = "http://www.famousbirthdays.com/" . $bday;
+$base = "http://www.famousbirthdays.com/" . $bday;
 
 echo "Celebs Birthday! "	;
-
+/*
 // Retrieve the DOM from a given URL
 $html = file_get_html($url);
 
 // Find all SPAN tags that have a class of "myClass"
 foreach($html->find('a.celeb') as $e)
   	echo $e->outertext . '<br>';
-  	
+  */	
  
+ 
+ 
+ //$base = 'https://play.google.com/store/apps';
 
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curl, CURLOPT_URL, $base);
+curl_setopt($curl, CURLOPT_REFERER, $base);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+$str = curl_exec($curl);
+curl_close($curl);
+
+// Create a DOM object
+$html_base = new simple_html_dom();
+// Load HTML from a string
+$html_base->load($str);
+
+//get all category links
+foreach($html_base->find('a.celeb') as $element) {
+    //echo "<pre>";
+    //print_r( $element->href );
+	echo $element->outertext . '<br>'; 
+    //echo "</pre>";
+}
+
+$html_base->clear(); 
+unset($html_base);s
+ 
+ 
 //echo "ok!!!!!";
 
 ?>
+
